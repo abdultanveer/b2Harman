f78766d66e33ee54bbcafadc6ce713c783c681cd
abdul <abdul.tanveer@gmail.com>
2022-06-06 12:02:24 +0530

    Initial commit

---
+++ app/src/main/java/com/abdul/b2harman/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.abdul.b2harman.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
